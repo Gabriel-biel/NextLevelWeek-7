cc7545dfc3790c3bc803e9e54c135ef41462e300
Gabriel-biel <gabriel97gla98@gmail.com>
2021-11-13 17:04:17 -0300

    Criado o Contexto (context) atribuido esse context em volta da aplicação, realizada as devidas alterações para esses fins

--- web/src/components/LoginBox/index.tsx
+++ web/src/components/LoginBox/index.tsx
@@ -1,15 +1,19 @@
 import Style from "./Style.module.scss";
 import { VscGithubInverted } from "react-icons/vsc"
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/auth";
 
 export function LoginBox (){
+  const { signinUrl, user } = useContext(AuthContext)
+
   return (
     <div className={Style.loginWrapper}>
       <strong >Entre e compartilhe sua menssagem</strong>
-      <a href="#" className={Style.signInWidthGithub}>
+      <a href={signinUrl} className={Style.signInWidthGithub}>
         <VscGithubInverted size="24"/>
         Entrar com github
       </a>
-
+      
     </div>
   )
 }
